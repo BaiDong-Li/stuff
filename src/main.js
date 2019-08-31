8ec8820aeb889a8c84589f31290af161061a5f17
libaidong <1134595193@qq.com>
2019-08-31 10:26:24 +0800

    初始化项目 安装elemeentui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+
+// 缺一条引入样式
 
 Vue.config.productionTip = false
+Vue.use(Element)// 全局注册 任职位置都可以使用
 
 new Vue({
   router,
