5a4af250a6f9b6489ae679f686e35b378eb172f6
libaidong <1134595193@qq.com>
2019-08-31 19:26:39 +0800

    开始修改前的最后一次提交

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './style/index.less' // 引入初始化样式
+import axios from 'axios'
 
+Vue.prototype.$axios = axios // 使用axios 为vue对象的原型使用axios
+// Vue.use(axios)
 Vue.config.productionTip = false
 Vue.use(Element)// 全局注册 任职位置都可以使用
 
