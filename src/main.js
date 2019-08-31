572725ebc8c8514db89c1ccfab5f235856c275ca
libaidong <1134595193@qq.com>
2019-08-31 19:59:13 +0800

    初始化结束

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './style/index.less' // 引入初始化样式
 import axios from 'axios'
-
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值基础地址
 Vue.prototype.$axios = axios // 使用axios 为vue对象的原型使用axios
-// Vue.use(axios)
+
 Vue.config.productionTip = false
 Vue.use(Element)// 全局注册 任职位置都可以使用
 
