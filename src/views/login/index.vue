b00cf3cfcf881ad734e1523488097880327fe7a5
libaidong <1134595193@qq.com>
2019-09-01 14:47:23 +0800

    跟着做的今天还要再做一次

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,115 @@
 <template>
-  <div>
-
-      login
+  <div class="login">
+      <el-card class="login-card"  >
+          <div class=" logo"  ><img src="../../assets/image/logo_index.png" alt=""></div>
+          <!-- 表单容器 -->
+          <el-form ref="abc"  style="margin-top:20px" :model="loginForm" :rules="loginRules">
+              <!-- 手机号 -->
+              <el-form-item prop="mobile" >
+                  <el-input placeholder="请输入手机号" v-model='loginForm.mobile'   ></el-input>
+              </el-form-item>
+              <!-- 验证码 -->
+              <el-form-item prop="code" >
+                  <el-input style="width:280px" placeholder="请输入验证码" v-model="loginForm.code" ></el-input>
+                  <el-button style="float:right" type="info" >发送验证码</el-button>
+              </el-form-item>
+              <!-- 确认框 -->
+               <el-form-item prop="check" >
+                    <el-checkbox v-model="loginForm.check">亲爱的用户我是你爹</el-checkbox>
+               </el-form-item>
+               <!-- 确认框 -->
+               <el-button  @click='login' type="primary" style="width:100%"   >确认登陆</el-button>
+          </el-form>
+      </el-card>
   </div>
 </template>
 
 <script>
+
 export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      if (value) {
+        callBack() // 如果 value 为true直接通过
+      } else {
+        callBack(new Error('唉不懂'))
+      }
+    }
+
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: true
+
+      },
+      loginRules: {
+        mobile: [
+          {
+            required: true,
+            message: '手机号不能为空'
+          }, {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式不正确'
+          }
+        ],
+        code: [
+          { required: true,
+            message: '验证码不能为空' },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码需要为六位数字'
+          }
+        ],
+        check: [{ validator }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.abc.validate(
+        IsOk => {
+          if (IsOk) {
+            this.$axios({
+              url: '/authorizations',
+              method: 'post',
+              data: this.abc
+            }).then(result => {
+              // 把token令牌放在前端缓存
+              window.localStorage.setItem('user-token', result.data.data.token)
+              // 成功就回到主页
+              this.$router.push('/')
+            }).catch(
+              () => this.$message({ message: '手机号验证码有错', type: 'warning' })
+            )
+          }
+        }
+      )
+    }
+  }
 
 }
+
 </script>
 
-<style>
+<style lang='less'  scoped >
+.login {
+  background: url("../../assets/image/login_bg.jpg");
+  height: 100vh;
+  background-size: cover;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-card{
+    height:340px;
+    width: 450px;
+    .logo{
+        text-align:center;
+        img{
+            height:40px;
 
+        }
+    }
+}
 </style>
